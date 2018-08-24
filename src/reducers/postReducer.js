86f2bd98c5a445cbb94b4c4020cc5f6c8bb32167
keef-ui <keef-v@hotmail.co.uk>
2018-08-24 01:21:26 +0100

    added file structure for redux

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,31 @@
+import { FETCH_POSTS, NEW_POST } from '../actions/types';
+
+//1]add new  states here and initialise. ie detail:{test:'a'}
+const initialState = {
+  items: [],
+  item: {},
+  detail:{}
+
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload
+      };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
+      case "GET_DETAIL":
+      return {
+        ...state,
+        detail: action.payload
+      };
+    default:
+      return state;
+  }
+}
